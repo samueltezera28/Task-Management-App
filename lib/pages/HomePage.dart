1d3c71600bde97cce4cbf87518f77df712d479ed
Samuel Tezera <99389679+samueltezera28@users.noreply.github.com>
2024-09-07 14:27:01 +0300

    feat: Add lazy loading for images and fetch random quotes on Home page

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,69 +1,110 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:task_management_app/model/quoteService.dart';
 import 'package:task_management_app/model/taskProvider.dart';
 import 'package:task_management_app/pages/AddTaskPage.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String _quote = 'Fetching quote...';
+  final QuoteService _quoteService = QuoteService();
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchQuote();
+  }
+
+  Future<void> _fetchQuote() async {
+    try {
+      final quote = await _quoteService.fetchQuote();
+      setState(() {
+        _quote = quote;
+      });
+    } catch (e) {
+      setState(() {
+        _quote = 'Oops! No internet. Even the best of us need a break!';
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Consumer<TaskProvider>(
-        builder: (context, taskProvider, child) {
-          if (taskProvider.tasks.isEmpty) {
-            return const Center(child: Text('No tasks available'));
-          } else {
-            return ListView.builder(
-              itemCount: taskProvider.tasks.length,
-              itemBuilder: (context, index) {
-                final task = taskProvider.tasks[index];
-                return ListTile(
-                  leading: Checkbox(
-                    value: task['isCompleted'] == 1,
-                    onChanged: (bool? value) {
-                      final updatedTask = {
-                        ...task,
-                        'isCompleted': value! ? 1 : 0,
-                      };
-                      taskProvider.updateTask(updatedTask);
-                    },
-                  ),
-                  title: Text(
-                    task['title'],
-                    style: TextStyle(
-                      decoration: task['isCompleted'] == 1
-                          ? TextDecoration.lineThrough
-                          : TextDecoration.none,
-                    ),
-                  ),
-                  subtitle: Text(
-                    '${task['description']}\nDue Date: ${task['dueDate']}\nStatus: ${task['isCompleted'] == 1 ? 'Completed' : 'Incomplete'}',
-                  ),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      IconButton(
-                        icon: Icon(Icons.edit),
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) =>
-                                  AddEditTaskScreen(task: task),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              '"${_quote}"',
+              style: TextStyle(fontSize: 18, fontStyle: FontStyle.italic),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Expanded(
+            child: Consumer<TaskProvider>(
+              builder: (context, taskProvider, child) {
+                if (taskProvider.tasks.isEmpty) {
+                  return const Center(child: Text('No tasks available'));
+                } else {
+                  return ListView.builder(
+                    itemCount: taskProvider.tasks.length,
+                    itemBuilder: (context, index) {
+                      final task = taskProvider.tasks[index];
+                      return ListTile(
+                        leading: Checkbox(
+                          value: task['isCompleted'] == 1,
+                          onChanged: (bool? value) {
+                            final updatedTask = {
+                              ...task,
+                              'isCompleted': value! ? 1 : 0,
+                            };
+                            taskProvider.updateTask(updatedTask);
+                          },
+                        ),
+                        title: Text(
+                          task['title'],
+                          style: TextStyle(
+                            decoration: task['isCompleted'] == 1
+                                ? TextDecoration.lineThrough
+                                : TextDecoration.none,
+                          ),
+                        ),
+                        subtitle: Text(
+                          '${task['description']}\nDue Date: ${task['dueDate']}\nStatus: ${task['isCompleted'] == 1 ? 'Completed' : 'Incomplete'}',
+                        ),
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            IconButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              AddEditTaskScreen(task: task)));
+                                },
+                                icon: Icon(Icons.edit)),
+                            IconButton(
+                              onPressed: () {
+                                taskProvider.deleteTask(task['id']);
+                              },
+                              icon: Icon(Icons.delete),
                             ),
-                          );
-                        },
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.delete),
-                        onPressed: () => taskProvider.deleteTask(task['id']),
-                      ),
-                    ],
-                  ),
-                );
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                }
               },
-            );
-          }
-        },
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
