db51c7edd2e355f196ee7cb73e274e8b995157a8
Samuel Tezera <99389679+samueltezera28@users.noreply.github.com>
2024-09-07 11:50:51 +0300

    chore: Update CompletedTaskPage to display completed tasks using TaskProvider

--- lib/pages/CompletedTaskPage.dart
+++ lib/pages/CompletedTaskPage.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:task_management_app/model/taskProvider.dart';
 
 class CompletedTasks extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Completed Tasks Screen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Completed Tasks'),
+      ),
+      body: Consumer<TaskProvider>(
+        builder: (context, taskProvider, child) {
+          final completedTasks = taskProvider.tasks
+              .where((task) => task['isCompleted'] == 1)
+              .toList();
+
+          if (completedTasks.isEmpty) {
+            return Center(child: Text('No completed tasks available'));
+          } else {
+            return ListView.builder(
+              itemCount: completedTasks.length,
+              itemBuilder: (context, index) {
+                final task = completedTasks[index];
+                return ListTile(
+                  title: Text(
+                    task['title'],
+                    style: TextStyle(
+                      decoration: TextDecoration.lineThrough,
+                    ),
+                  ),
+                  subtitle: Text(
+                    '${task['description']}\nDue Date: ${task['dueDate']}\nStatus: Completed',
+                  ),
+                );
+              },
+            );
+          }
+        },
+      ),
     );
   }
 }
